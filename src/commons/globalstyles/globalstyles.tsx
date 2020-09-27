0f94e976e610b4bc23b6563e93bb000c23b7a722
Hernan <hernan.faifman@gmail.com>
2020-09-27 20:59:03 -0300

    added background to body

--- src/commons/globalstyles/globalstyles.tsx
+++ src/commons/globalstyles/globalstyles.tsx
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-
+body{
+  background: #E5E5E5;
+};
   body, * {
+   
     margin: 0;
     padding: 0;
     font-family: 'Source Sans Pro', sans-serif, Helvetica, Sans-Serif;
