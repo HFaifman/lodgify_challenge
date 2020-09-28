305c4f18b224489bb20c3dd40f888e98d486acfb
Hernan Ariel Faifman <hernan.faifman@globant.com>
2020-09-28 14:44:38 -0300

    changed border size in card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -26,7 +26,7 @@ const StyledArticle = styled.article`
   border-radius: 5px;
 
   &:hover {
-    border: 0.5px solid rgba(51, 51, 51, 1);
+    border: 1px solid rgba(51, 51, 51, 1);
     box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.15);
   }
 `;
