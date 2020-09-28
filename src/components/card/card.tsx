a69953c3e07ae33a4511150438d6e4e90852e737
Hernan <hernan.faifman@gmail.com>
2020-09-27 21:46:32 -0300

    changed box-shadow on hover in card

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -20,6 +20,7 @@ const StyledArticle = styled.article`
   &:hover {
     border: 1px solid rgba(51, 51, 51, 1);
     border-radius: 5px;
+    box-shadow: 0px 10px 20px 0px rgba(0, 0, 0, 0.15);
   }
 `;
 
